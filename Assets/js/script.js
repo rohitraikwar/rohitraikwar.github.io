26db38e482c294f832d38cc902b5e481d5c3cbe6
rohitraikwar <rohitraikwar363@gmail.com>
2023-07-05 16:51:43 +0530

    update form

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -27,3 +27,37 @@ var swiper = new Swiper(".slide-content", {
         },
     },
   });
+
+
+  // Form
+  function message(){
+    var Name = document.getElementById('name');
+    var email = document.getElementById('email');
+    var msg = document.getElementById('mess');
+    var msg = document.getElementById('tel');
+    const success = document.getElementById('success');
+    const danger = document.getElementById('danger');
+  
+    if(Name.value === '' || email.value === '' || mess.value === '' | tel.value === ''){
+        danger.style.display = 'block';
+    }
+    else{
+        setTimeout(() => {
+            Name.value = '';
+            email.value = '';
+            mess.value = '';
+            tel.value = '';
+
+
+        }, 2000);
+  
+        success.style.display = 'block';
+    }
+  
+  
+    setTimeout(() => {
+        danger.style.display = 'none';
+        success.style.display = 'none';
+    }, 4000);
+  
+  }
